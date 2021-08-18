f14461d4dfc9a9806fdc9839dc7bed428989e5fa
Mark Mansolino <markjm610@gmail.com>
2021-08-18 19:09:54 -0400

    Change order of messages to display the most recent on the bottom by changing the Sequelize query"

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,5 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
 
 const db = new Sequelize('messenger', 'hatchways_user', 'hatchways_password', {
   host: 'localhost',
--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Conversation, Message } = require("../../db/models");
-const { Op } = require("sequelize");
+const { Op , fn, col, literal} = require("sequelize");
 const onlineUsers = require("../../onlineUsers");
 
 // get all conversations for a user, include latest message text for preview, and all messages
@@ -19,9 +19,14 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [
+        ["createdAt", "DESC"],
+        [Message, "createdAt", "ASC"],
+      ],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message,
+          // order: ["createdAt", "ASC"]
+        },
         {
           model: User,
           as: "user1",
@@ -47,6 +52,7 @@ router.get("/", async (req, res, next) => {
       ],
     });
 
+
     for (let i = 0; i < conversations.length; i++) {
       const convo = conversations[i];
       const convoJSON = convo.toJSON();
@@ -68,10 +74,10 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
     }
-
+    
     res.json(conversations);
   } catch (error) {
     next(error);
