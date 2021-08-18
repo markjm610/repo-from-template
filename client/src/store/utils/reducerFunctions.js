ed7d725463dc89590e6c3a824ae5743588919e20
Mark Mansolino <markjm610@gmail.com>
2021-08-18 15:00:38 -0400

    Convert postMessage to async function and await saveMessage so store gets updated with message data

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -10,7 +10,7 @@ export const addMessageToStore = (state, payload) => {
     newConvo.latestMessageText = message.text;
     return [newConvo, ...state];
   }
-
+  
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
       convo.messages.push(message);
