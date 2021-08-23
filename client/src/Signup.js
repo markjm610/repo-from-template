c40badd17fdea6cac6e3aa273c5a887e861cb35c
Mark Mansolino <markjm610@gmail.com>
2021-08-23 17:35:52 -0400

    Commit before switching branches

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -70,77 +70,79 @@ const Login = (props) => {
     <Grid container className={classes.root} direction="row">
       <Grid item>
         <Box className={classes.imageBox}>
-          <img src={sideImage} alt="" className={classes.sideImage}/>
+          <img src={sideImage} alt="" className={classes.sideImage} />
           {/* <div className={classes.sideImageOverlay} /> */}
         </Box>
       </Grid>
       <Grid item>
-        <Box className={classes.formBox}>
-          <Grid container item>
+        <Grid container justify="center">
+          <Grid item>
             <Typography>Create an account.</Typography>
             <Typography>Already have an account?</Typography>
             <Button onClick={() => history.push("/login")}>Login</Button>
           </Grid>
-          <form onSubmit={handleRegister}>
-            <Grid>
+          <Grid item>
+            <form onSubmit={handleRegister}>
               <Grid>
-                <FormControl>
-                  <TextField
-                    aria-label="username"
-                    label="Username"
-                    name="username"
-                    type="text"
-                    required
-                  />
-                </FormControl>
+                <Grid>
+                  <FormControl>
+                    <TextField
+                      aria-label="username"
+                      label="Username"
+                      name="username"
+                      type="text"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl>
+                    <TextField
+                      label="E-mail address"
+                      aria-label="e-mail address"
+                      type="email"
+                      name="email"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl error={!!formErrorMessage.confirmPassword}>
+                    <TextField
+                      aria-label="password"
+                      label="Password"
+                      type="password"
+                      inputProps={{ minLength: 6 }}
+                      name="password"
+                      required
+                    />
+                    <FormHelperText>
+                      {formErrorMessage.confirmPassword}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl error={!!formErrorMessage.confirmPassword}>
+                    <TextField
+                      label="Confirm Password"
+                      aria-label="confirm password"
+                      type="password"
+                      inputProps={{ minLength: 6 }}
+                      name="confirmPassword"
+                      required
+                    />
+                    <FormHelperText>
+                      {formErrorMessage.confirmPassword}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Button type="submit" variant="contained" size="large">
+                  Create
+                </Button>
               </Grid>
-              <Grid>
-                <FormControl>
-                  <TextField
-                    label="E-mail address"
-                    aria-label="e-mail address"
-                    type="email"
-                    name="email"
-                    required
-                  />
-                </FormControl>
-              </Grid>
-              <Grid>
-                <FormControl error={!!formErrorMessage.confirmPassword}>
-                  <TextField
-                    aria-label="password"
-                    label="Password"
-                    type="password"
-                    inputProps={{ minLength: 6 }}
-                    name="password"
-                    required
-                  />
-                  <FormHelperText>
-                    {formErrorMessage.confirmPassword}
-                  </FormHelperText>
-                </FormControl>
-              </Grid>
-              <Grid>
-                <FormControl error={!!formErrorMessage.confirmPassword}>
-                  <TextField
-                    label="Confirm Password"
-                    aria-label="confirm password"
-                    type="password"
-                    inputProps={{ minLength: 6 }}
-                    name="confirmPassword"
-                    required
-                  />
-                  <FormHelperText>
-                    {formErrorMessage.confirmPassword}
-                  </FormHelperText>
-                </FormControl>
-              </Grid>
-              <Button type="submit" variant="contained" size="large">
-                Create
-              </Button>
-            </Grid>
-          </form>
-        </Box>
+            </form>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
