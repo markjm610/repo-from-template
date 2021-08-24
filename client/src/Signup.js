3f0acf5835179d4ab2a50ee0eaec15b600f78949
Mark Mansolino <markjm610@gmail.com>
2021-08-24 03:26:58 -0400

    Left background image and gradient

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -21,38 +21,40 @@ const useStyles = makeStyles((theme) => ({
   },
   imageBox: {
     height: '100vh',
-    width: '100%'
+    width: '100%',
+    position: 'relative'
   },
   sideImage: {
     height: '100vh',
     width: '100%',
     zIndex: 1,
+    position: 'absolute'
   },
   sideImageOverlay: {
     height: '100%',
     width: '100%',
-    backgroundColor: 'blue',
-    zIndex: 2,
-    position: 'absolute'
-    // opacity: '85%'
+    backgroundImage: `linear-gradient(rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)), url(${sideImage})`,
+    backgroundSize: 'cover',
   },
   rightSide: {
     height: '100%',
     width: '100%'
   },
+  switchFormText: {
+    color: "#9CADC8"
+  },
   switchFormButton: {
     backgroundColor: "white",
     color: '#3A8DFF',
-    boxShadow: '1px 1px 1px gray'
+    boxShadow: '0px 0px 10px 2px lightgray',
+    height: 60,
+    width: 140
   },
   formHeading: {
     fontWeight: '900',
   },
   form: {
     width: '100%',
-    // display: 'flex',
-    // justifyContent: 'center',
-    // flexDirection: 'column'
   },
   formControl: {
     width: '100%'
@@ -60,9 +62,11 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     width: '100%'
   },
-  button: {
+  submitButton: {
     backgroundColor: "#3A8DFF",
-    color: 'white'
+    color: 'white',
+    height: 60,
+    width: 160
   }
 }));
 
@@ -94,86 +98,108 @@ const Login = (props) => {
   return (
     <Grid container className={classes.root}>
       <Grid item md={5} lg={5}>
-        <Box className={classes.imageBox}>
-          <img src={sideImage} alt="" className={classes.sideImage} />
-          {/* <div className={classes.sideImageOverlay} /> */}
-        </Box>
+        {/* <Box className={classes.imageBox}>
+          <img src={sideImage} alt="" className={classes.sideImage} /> */}
+        <div className={classes.sideImageOverlay} />
+        {/* </Box> */}
       </Grid>
       <Grid item md={7} lg={7} className={classes.rightSide} container justify="center">
-        <Grid item md={12} lg={12} container justify="flex-end" alignItems="center">
+        <Grid item md={12} lg={12} container justify="flex-end" alignItems="center" spacing={5}>
           <Grid item>
-            <Typography>Already have an account?</Typography>
+            <Typography className={classes.switchFormText}>Already have an account?</Typography>
           </Grid>
           <Grid item>
             <Button className={classes.switchFormButton} onClick={() => history.push("/login")}>Login</Button>
           </Grid>
         </Grid>
-        <Grid item md={8} lg={8} container justify="center">
+        <Grid item md={8} lg={8}>
           <form className={classes.form} onSubmit={handleRegister}>
-            <Typography variant="h4" className={classes.formHeading}>Create an account.</Typography>
-            <Grid item md={12} lg={12}>
-              <FormControl className={classes.formControl}>
-                <TextField
-                  InputLabelProps={{ required: false }}
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                  className={classes.textField}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item md={12} lg={12}>
-              <FormControl className={classes.formControl}>
-                <TextField
-                  InputLabelProps={{ required: false }}
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                  className={classes.textField}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item md={12} lg={12}>
-              <FormControl error={!!formErrorMessage.confirmPassword} className={classes.formControl}>
-                <TextField
-                  InputLabelProps={{ required: false }}
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                  className={classes.textField}
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid item md={12} lg={12}>
-              <FormControl error={!!formErrorMessage.confirmPassword} className={classes.formControl}>
-                <TextField
-                  InputLabelProps={{ required: false }}
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                  className={classes.textField}
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid item container justify="center" lg={12}>
+            <Grid container spacing={6}>
               <Grid item>
-                <Button type="submit" variant="contained" size="large" className={classes.button}>
+                <Typography variant="h4" className={classes.formHeading}>Create an account.</Typography>
+              </Grid>
+              <Grid item md={12} lg={12}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    InputLabelProps={{
+                      required: false,
+                      style: {
+                        color: "#9CADC8"
+                      }
+                    }}
+                    aria-label="username"
+                    label="Username"
+                    name="username"
+                    type="text"
+                    required
+                    className={classes.textField}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item md={12} lg={12}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    InputLabelProps={{
+                      required: false,
+                      style: {
+                        color: "#9CADC8"
+                      }
+                    }}
+                    label="E-mail address"
+                    aria-label="e-mail address"
+                    type="email"
+                    name="email"
+                    required
+                    className={classes.textField}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item md={12} lg={12}>
+                <FormControl error={!!formErrorMessage.confirmPassword} className={classes.formControl}>
+                  <TextField
+                    InputLabelProps={{
+                      required: false,
+                      style: {
+                        color: "#9CADC8"
+                      }
+                    }}
+                    aria-label="password"
+                    label="Password"
+                    type="password"
+                    inputProps={{ minLength: 6 }}
+                    name="password"
+                    required
+                    className={classes.textField}
+                  />
+                  <FormHelperText>
+                    {formErrorMessage.confirmPassword}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid item md={12} lg={12}>
+                <FormControl error={!!formErrorMessage.confirmPassword} className={classes.formControl}>
+                  <TextField
+                    InputLabelProps={{
+                      required: false,
+                      style: {
+                        color: "#9CADC8"
+                      }
+                    }}
+                    label="Confirm Password"
+                    aria-label="confirm password"
+                    type="password"
+                    inputProps={{ minLength: 6 }}
+                    name="confirmPassword"
+                    required
+                    className={classes.textField}
+                  />
+                  <FormHelperText>
+                    {formErrorMessage.confirmPassword}
+                  </FormHelperText>
+                </FormControl>
+              </Grid>
+              <Grid item container justify="center" lg={12}>
+                <Button type="submit" variant="contained" size="large" className={classes.submitButton}>
                   Create
                 </Button>
               </Grid>
