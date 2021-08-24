a351920a928f504ea07a6d3da9f772634f29a7da
Mark Mansolino <markjm610@gmail.com>
2021-08-24 19:22:32 -0400

    Remove comments

--- client/src/components/LandingPage/Signup/index.js
+++ client/src/components/LandingPage/Signup/index.js
@@ -1,7 +1,3 @@
-// export { default as SideImage } from "./SideImage";
-// export { default as SignUpForm } from "./SignupForm";
-// export { default as Signup } from "./Signup";
-
 import Signup from './Signup';
 
 export default Signup;
