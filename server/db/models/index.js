a30a8ef1152f6f3e5b1359f0fa2f3fcf731b9278
Mark Mansolino <markjm610@gmail.com>
2021-08-26 21:41:55 -0400

    Removing comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,6 @@ const Message = require("./message");
 
 // associations
 
-// Conversation and users many to many
-// Messages have to be read by specific users
-
 Conversation.belongsToMany(User, { through: 'UserConversation' });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
